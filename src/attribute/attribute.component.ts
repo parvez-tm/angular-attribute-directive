ecadb96904ac545eecfb8283f7a7d6dfc78d7361
parvez-tm <parvez.tm@outlook.com>
2025-03-20 11:14:49 +0530

    attribute directive added

---
+++ src/attribute/attribute.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attribute',
+  imports: [],
+  templateUrl: './attribute.component.html',
+  styleUrl: './attribute.component.css'
+})
+export class AttributeComponent {
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { AttributeComponent } from './attribute/attribute.component';
 
 @Component({
   selector: 'app-root',
+  imports: [AttributeComponent],
   template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
+   <app-attribute />
   `,
 })
 export class App {
-  name = 'Angular';
+
 }
 
 bootstrapApplication(App);
