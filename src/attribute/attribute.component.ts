4143c0d34504d2c0f75be38729da4af95f23a45b
parvez-tm <parvez.tm@outlook.com>
2025-03-20 12:00:56 +0530

    logic working with bad css

--- src/attribute/attribute.component.ts
+++ src/attribute/attribute.component.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-attribute',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './attribute.component.html',
   styleUrl: './attribute.component.css'
 })
 export class AttributeComponent {
 
+  ind:number[] = []
+  num:number = 0
+  constructor(){
+    for (let index = 1; index <= 50; index++) {
+      this.ind.push(index)
+    }
+  }
+
+  divisible(num:number){
+    this.num = num
+  }
 }
